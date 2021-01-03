650d965f0d386e4a5c20947984aab5a6b18db6e7
tzvikaco4github <tzvikaco@gmail.com>
2021-01-03 18:34:22 +0200

    Adding Supplier

---
+++ src/main/java/net/tzvikaco/fruitshop/domain/internal/Supplier.java
@@ -0,0 +1,53 @@
+package net.tzvikaco.fruitshop.domain.internal;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import static java.time.LocalDate.now;
+
+@Document(collection = "supplier")
+public class Supplier extends Person {
+
+    @Field("start")
+    private LocalDate start;
+
+    @Field("end")
+    private LocalDate end;
+
+    public LocalDate getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDate start) {
+        this.start = start;
+    }
+
+    public LocalDate getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDate end) {
+        this.end = end;
+    }
+
+    public boolean isActive() {
+        return start != null && !start.isAfter(now()) && (end == null || end.isAfter(now()));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Supplier supplier = (Supplier) o;
+        return Objects.equals(start, supplier.start) && Objects.equals(end, supplier.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), start, end);
+    }
+}
---
+++ src/test/java/net/tzvikaco/fruitshop/domain/internal/SupplierTest.java
@@ -0,0 +1,62 @@
+package net.tzvikaco.fruitshop.domain.internal;
+
+import net.tzvikaco.fruitshop.web.rest.TestUtil;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SupplierTest {
+
+    @Test
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Supplier.class);
+        Supplier supplier1 = new Supplier();
+        supplier1.setId("id1");
+        Supplier supplier2 = new Supplier();
+        supplier2.setId(supplier1.getId());
+        assertThat(supplier1).isEqualTo(supplier2);
+        supplier2.setId("id2");
+        assertThat(supplier1).isNotEqualTo(supplier2);
+        supplier1.setId(null);
+        assertThat(supplier1).isNotEqualTo(supplier2);
+    }
+
+    @Test
+    public void testIsActive_whenStartDateIsNull_thenFalse() {
+        Supplier supplier = new Supplier();
+        assertThat(supplier.isActive()).isFalse();
+    }
+
+    @Test
+    public void testIsActive_whenStartDateIsAfterNow_thenFalse() {
+        Supplier supplier = new Supplier();
+        supplier.setStart(LocalDate.now().plus(1, ChronoUnit.DAYS));
+        assertThat(supplier.isActive()).isFalse();
+    }
+
+    @Test
+    public void testIsActive_whenStartDateIsNotAfterNowAndEndDateIsNull_thenTrue() {
+        Supplier supplier = new Supplier();
+        supplier.setStart(LocalDate.now().minus(1, ChronoUnit.DAYS));
+        assertThat(supplier.isActive()).isTrue();
+    }
+
+    @Test
+    public void testIsActive_whenStartDateIsNotAfterNowAndEndDateIsBeforeToday_thenFalse() {
+        Supplier supplier = new Supplier();
+        supplier.setStart(LocalDate.now().minus(2, ChronoUnit.DAYS));
+        supplier.setEnd(LocalDate.now().minus(1, ChronoUnit.DAYS));
+        assertThat(supplier.isActive()).isFalse();
+    }
+
+    @Test
+    public void testIsActive_whenStartDateIsBeforeNowAndEndDateIsAfterToday_thenTrue() {
+        Supplier supplier = new Supplier();
+        supplier.setStart(LocalDate.now().minus(1, ChronoUnit.DAYS));
+        supplier.setEnd(LocalDate.now().plus(1, ChronoUnit.DAYS));
+        assertThat(supplier.isActive()).isTrue();
+    }
+}
